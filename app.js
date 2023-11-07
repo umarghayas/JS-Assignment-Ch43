375540b982313561b74c356353bb2293e8859947
Umar Ghayas <umar.ghayas2000@gmail.com>
2023-11-07 18:48:54 +0500

    Assignment # 43-48 DONE

---
+++ app.js
@@ -0,0 +1,58 @@
+// Assignment # 43-48
+// JAVASCRIPT
+
+// 1. Show an alert box on click on a link
+
+// function utho(){
+//     alert("Utho Ghar Jao")
+// }  //DONE
+
+
+// 2. Display some Mobile images in browser. On click on an 
+// image Show the message in alert to user
+
+// function purchase(){
+//     alert("Thank you for purchasing this phone!")
+// }   //DONE
+
+// 3. Display 10 student records in table and each row should contain a delete 
+// button. If you click on a button to delete a record, entire row should be 
+// deleted.
+// var i=0;
+// var indexNo = document.getElementById("indexNo")
+// indexNo.innerHTML = i
+
+// function delRow(rowNum){
+//     var row = document.getElementById("row"+rowNum)
+//     row.innerHTML = "";
+// }  //DONE
+
+
+// 4. Display an image in browser. Change the picture on mouseover and set the
+// first picture on mouseout.
+
+// function changeIMG(ele, flag) {
+//     if (flag == 'enter') {
+//         ele.src = "https://steamuserimages-a.akamaihd.net/ugc/909046337463491130/AF58A14BD541F9572D8DD3F665F271F6CEEA4F6A/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
+//     }
+//     else{
+//         ele.src = "https://www.bmw-m.com/content/dam/bmw/marketBMW_M/common/topics/magazine-article-pool/2020/m3-f80-f82-portraet/bmw-m3-f80-di-01.jpg"
+//     }
+// }   //DONE
+
+// 5. Show a counter in browser. Counter should increase on click on increase 
+// button and decrease on click on decrease button. And show updated counter 
+// value in browser
+
+// function counter(change){
+//     var count = document.getElementById("count")
+//     var value = Number(count.value)
+//     if(change == 'inc'){
+//         count.value = value+1
+//     }
+//     else{
+//         count.value = value-1
+//     }
+// }   //DONE
+
+// Assignment # 43-48 DONE
